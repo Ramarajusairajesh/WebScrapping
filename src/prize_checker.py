ea28e32c2181e0d0c79922587203baa19177aee5
RAMARAJU SAI RAJESHH <87467385+Ramarajusairajesh@users.noreply.github.com>
2025-01-22 15:12:12 +0530

    Update prize_checker.py

--- src/prize_checker.py
+++ src/prize_checker.py
@@ -8,9 +8,19 @@ file = open("prices.txt", "a")
 
 
 def prize_comparision(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+   headers = {
+'authority': 'www.amazon.com',
+'pragma': 'no-cache',
+'cache-control': 'no-cache',
+'dnt': '1',
+'upgrade-insecure-requests': '1',
+'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'sec-fetch-site': 'none',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-dest': 'document',
+'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
 
     try:
         response = requests.get(url, headers=headers, timeout=10)
